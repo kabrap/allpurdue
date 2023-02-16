37087690df4cc23bae76f2e39b6eac8f0c027a95
yashnarain <agarw236@purdue.edu>
2023-02-16 13:42:25 -0500

    major: added hashing

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require('md5');
 
 const app = express();
 
@@ -25,11 +25,6 @@ const schema = new mongoose.Schema({
     password: String
 });
 
-schema.plugin(encrypt, { 
-    secret: process.env.SECRET,
-    encryptedFields: ['password']
-});
-
 // Simple User Collection
 const User = new mongoose.model("User", schema);
 
@@ -47,7 +42,7 @@ app.get('/register', function(req, res) {
 app.post('/register', function(req, res) {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function(err) {
         if (err) {
@@ -62,7 +57,7 @@ app.post('/register', function(req, res) {
 // POST Route for Login
 app.post('/login', function(req, res) {
     var username = req.body.username;
-    var userPass = req.body.password; 
+    var userPass = md5(req.body.password); 
 
     User.findOne({email: username}, function(err, userExists) {
             if (err) {
