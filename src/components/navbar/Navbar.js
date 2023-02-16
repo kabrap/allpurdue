70532a456dabed660ac1fcf5ad9a06c80d54877f
Antony <ni96@purdue.edu>
2023-02-15 19:10:26 -0500

    Folder setup

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,12 @@
+import './Navbar.css';
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='navbar-container'>
+      Navbar
+    </div>
+  )
+}
+
+export default Navbar
