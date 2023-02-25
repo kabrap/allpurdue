dfd2bc7b5d8608a827095fbe01f0f363689f997e
Antony <ni96@purdue.edu>
2023-02-25 14:37:04 -0500

    Frontend & backend integration for Categories page

--- src/components/card/CategoryCard.js
+++ src/components/card/CategoryCard.js
@@ -1,31 +1,38 @@
 import React from 'react';
 import './CategoryCard.css';
 import Chipotle from '../../images/chipotle.jpg'
+import { Link } from 'react-router-dom';
 
-const Card = () => {
+const Card = (props) => {
   return (
-    <div className="card">
-      <div className="card-image">
-        <img src={Chipotle} alt="restaurant" />
-      </div>
-      <div className="card-content">
-        <div className="restaurant-header">
-          <h2 className="restaurant-name">Chipotle</h2>
-          <div className="rating">
-            <span className="stars">&#9733;&#9733;&#9733;&#9733;&#9733;</span>
-            <span className="rating-number">&#40;5.0&#41;</span>
+    <Link key={props._id} to={`/places/${props.title}`}>
+      <div className="card">
+        <div className="card-image">
+          <img src={Chipotle} alt="restaurant" />
+        </div>
+        <div className="card-content">
+          <div className="restaurant-header">
+            <h2 className="restaurant-name">{props.title}</h2>
+            <div className="rating">
+              <span className="stars">&#9733;&#9733;&#9733;&#9733;&#9733;</span>
+              <span className="rating-number">&#40;{props.avgRating}&#41;</span>
+            </div>
+          </div>
+          <div className='tags-container'>
+            {props.tags.map(tag => (
+              <span id='tag'>{tag}</span>
+            ))}
+          </div>
+          <span className="review-placeholder">{props.description}</span>
+          <div className="card-button">
+            {props.placeType === 'Cafe' && <button>All cafes &#8594;</button>}
+            {props.placeType === 'Food' && <button>All restaurants &#8594;</button>}
+            {props.placeType === 'Living' && <button>All residence halls &#8594;</button>}
+            {props.placeType === 'Study' && <button>All study spots &#8594;</button>}
           </div>
         </div>
-        <div className='tags-container'>
-          <span id='tag'>Fast Food</span>
-          <span id='tag'>Mexican</span>
-        </div>
-        <span className="review-placeholder">A brief review created by the user here. I am just typing this now as a placeholder.</span>
-        <div className="card-button">
-          <button>All restaurants &#8594;</button>
-        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
