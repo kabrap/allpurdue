34e8a19ae119810086a0fa5609c17a7efe84b661
Matthew Stroup <mjstroup@purdue.edu>
2023-03-29 18:45:00 -0400

    Fix edit location style

--- src/pages/EditPlace.js
+++ src/pages/EditPlace.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import './EditLocation.css'
 import axios from 'axios'
 
 function EditPlace() {
@@ -181,72 +182,72 @@ function EditPlace() {
     }
 
     return (
-        <div className="location-container">
-            <h2 className="title">Edit a Location</h2>
-            <form id='forgot-form' className="form" onSubmit={handleSubmit}>
+        <div className="edit-location-container">
+            <h2 className="edit-location-title">Edit a Location</h2>
+            <form id='forgot-form' className="edit-location-form" onSubmit={handleSubmit}>
               <div className="type-bar">
                 {placeType === "Food" &&
-                  <button id='left-type' className='clicked-button' onClick={() => handlePlaceType("Food")}>Food</button>
+                  <button id='left-type' className='edit-location-clicked-button' onClick={() => handlePlaceType("Food")}>Food</button>
                 }
                 {placeType !== "Food" &&
-                  <button id='left-type' className='type-button' onClick={() => handlePlaceType("Food")}>Food</button>
+                  <button id='left-type' className='edit-location-type-button' onClick={() => handlePlaceType("Food")}>Food</button>
                 }
                 {placeType === "Cafe" &&
-                  <button className='clicked-button' onClick={() => handlePlaceType("Cafe")}>Cafe</button>
+                  <button className='edit-location-clicked-button' onClick={() => handlePlaceType("Cafe")}>Cafe</button>
                 }
                 {placeType !== "Cafe" &&
-                  <button className='type-button' onClick={() => handlePlaceType("Cafe")}>Cafe</button>
+                  <button className='edit-location-type-button' onClick={() => handlePlaceType("Cafe")}>Cafe</button>
                 }
                 {placeType === "Study" &&
-                  <button className='clicked-button'onClick={() => handlePlaceType("Study")}>Study</button>
+                  <button className='edit-location-clicked-button'onClick={() => handlePlaceType("Study")}>Study</button>
                 }
                 {placeType !== "Study" &&
-                  <button className='type-button'onClick={() => handlePlaceType("Study")}>Study</button>
+                  <button className='edit-location-type-button'onClick={() => handlePlaceType("Study")}>Study</button>
                 }
                 {placeType === "Living" &&
-                  <button id='right-type' className='clicked-button'onClick={() => handlePlaceType("Living")}>Living</button>
+                  <button id='right-type' className='edit-location-clicked-button'onClick={() => handlePlaceType("Living")}>Living</button>
                 }
                 {placeType !== "Living" &&
-                  <button id='right-type' className='type-button'onClick={() => handlePlaceType("Living")}>Living</button>
+                  <button id='right-type' className='edit-location-type-button'onClick={() => handlePlaceType("Living")}>Living</button>
                 }
               </div>
-              <label className='location-field-container'>
+              <label className='edit-location-field-container'>
                 <span>Name of Place</span>
                 <input type="message" value={nameOfPlace} onChange={handleNameOfPlace} placeholder="Name of place to edit..." required />
               </label>
-              <label className='location-field-container'>
+              <label className='edit-location-field-container'>
                 <span>Description of Place</span>
                 <textarea type="message" value={description} onChange={handleDescription} placeholder="Description of place to edit..." />
               </label>
-              <label className='location-field-container'>
+              <label className='edit-location-field-container'>
                 <span>Phone number</span>
                 <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" value={phoneNumber} onChange={handlePhoneNumber} placeholder="xxx-xxx-xxxx"></input>
               </label>
-              <label className='location-field-container'>
+              <label className='edit-location-field-container'>
                 <span>Tags</span>
                 <input type="text" placeholder="Fast Food, Chauncey, Chain" value={tags} pattern="[^,]+(,.*){3,}.+" onChange={handleTags}></input>
               </label>
-              <label className='location-field-container'>
+              <label className='edit-location-field-container'>
                 <span>Images</span>
                 <input type="url" placeholder="https://tinyurl.com/fiveguysfood, https://tinyurl.com/5guysinsidewl" value={images} onChange={handleImages}></input>
               </label>
-              <label className='location-field-container'>
+              <label className='edit-location-field-container'>
                 <span>Address</span>
                 <input type="text" placeholder="135 S Chauncey Ave Suite 1-K, West Lafayette, IN 47906" value={address} onChange={handleAddress}></input>
               </label>
-              <label className='location-field-container'>
+              <label className='edit-location-field-container'>
                 <span>Google Map Url</span>
                 <input type="url" placeholder="https://www.google.com/maps/place/Five+Guys/@40.4233731,-86.9122599,17.05z/data=!4m6!3m5!1s0x8812e2aed99721e9:0x2a65fca44b4f0116!8m2!3d40.4233321!4d-86.9080151!16s%2Fg%2F1tvq4qs0" value={googleUrl} onChange={handleGoogleUrl}></input>
               </label>
-              <label className='location-field-container'>
+              <label className='edit-location-field-container'>
                 <span>Latitude</span>
                 <input type="number" placeholder="40.42343994609269" value={latitude} onChange={handleLatitude}></input>
               </label>
-              <label className='location-field-container'>
+              <label className='edit-location-field-container'>
                 <span>Longitude</span>
                 <input type="number" placeholder="-86.90795109977412" value={longitude} onChange={handleLongitude}></input>
               </label>
-              <label className='location-field-container'>
+              <label className='edit-location-field-container'>
                 <span>Website</span>
                 <input type="url" placeholder="https://restaurants.fiveguys.com/135-S-Chauncey-Avenue" value={website} onChange={handleWebsite}></input>
               </label>
