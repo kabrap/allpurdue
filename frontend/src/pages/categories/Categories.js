69dacb98ec555be60b41ac240b296f01899baa41
Matthew Stroup <mjstroup@purdue.edu>
2023-04-16 20:35:01 -0400

    price filter

--- frontend/src/pages/categories/Categories.js
+++ frontend/src/pages/categories/Categories.js
@@ -6,12 +6,52 @@ import axios from 'axios'
 
 function Categories() {
   const [places, setPlaces] = useState([])
+  const [filterButton, setFilterButton] = useState(false)
   const [trending, setTrending] = useState([])
+  const [priceFilter, setPriceFilter] = useState([])
+  const [priceFilterTrending, setPriceFilterTrending] = useState([])
   const [displayTrending, setDisplayTrending] = useState(false)
+  const [lowFilterValue, setLowFilterValue] = useState('')
+  const [highFilterValue, setHighFilterValue] = useState('')
+
+  const handleFilterButton = () => {
+    setFilterButton(!filterButton)
+  }
 
   const handleTrendingClick = () => {
     setDisplayTrending(!displayTrending)
   };
+
+  const clearFields = () => {
+    setLowFilterValue('');
+    setHighFilterValue('');
+    setFilterButton(false)
+  }
+  
+  const handleLowFilterValue = (e) => {
+    setLowFilterValue(e.target.value)
+    if (e.target.value < 0) {
+      setLowFilterValue(0);
+    }
+    if (highFilterValue == '' || parseInt(e.target.value) > parseInt(highFilterValue)) {
+      setHighFilterValue(e.target.value);
+    }
+  }
+
+  const handleHighFilterValue = (e) => {
+    console.log(e.target.value)
+    setHighFilterValue(e.target.value)
+    if (parseInt(e.target.value) < 0) {
+      setHighFilterValue(0)
+    }
+    if (parseInt(e.target.value) < parseInt(lowFilterValue)) {
+      if (parseInt(e.target.value) >= 0) {
+        setLowFilterValue(e.target.value);
+      } else {
+        setLowFilterValue(0)
+      }
+    }
+  }
 
   useEffect(() => {
     async function fetchData() {
@@ -22,7 +62,7 @@ function Categories() {
         console.log("Trending here")
         console.log(trendingData.data)
         setPlaces(response.data);
-        setTrending(trendingData.data)
+        setTrending(trendingData.data);
       } catch (error) {
         console.error(error);
       }
@@ -42,11 +82,32 @@ function Categories() {
         <Link to='/categories/residence-halls'><button className='filter-button'>Residence Halls</button></Link>
         <Link to='/categories/study-spots'><button id='right-filter' className='filter-button'>Study Spots</button></Link>
       </div>
+      {filterButton &&
+        <div className='filter-form'>
+          <form className="filter-form-inputs">
+            <label>From </label>
+            <input type="number" placeholder="8" value={lowFilterValue} onChange={handleLowFilterValue}></input>
+            <br/>
+            <label>To &nbsp;&nbsp;&nbsp;&nbsp;</label>
+            <input type="number" placeholder="25" value={highFilterValue} onChange={handleHighFilterValue}></input>
+          </form>
+        </div>
+      }
       <div className='sorting-container'>
         <span id='all-places'>All Places {displayTrending ? '>' : ''} {displayTrending ? 'Trending' : ''}</span>
-        <button className="fire-button" onClick={handleTrendingClick}>
-          Trending
-        </button>
+        <div className='filter-button-container'>
+          {lowFilterValue + highFilterValue != '' &&
+            <div>
+              <button className='price-filter-button' onClick={clearFields}>Clear Filter</button>
+            </div>
+          }
+          <button className='price-filter-button' onClick={handleFilterButton}>
+            Price Filter
+          </button>
+          <button className="fire-button" onClick={handleTrendingClick}>
+            Trending
+          </button>
+        </div>
       </div>
       <div className='categories-cards'>
         {!displayTrending && places.map(place => (
