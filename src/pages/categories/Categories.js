58f9b261baefe481a0e08c1c1845ea82ca66f5fc
unknown <ni95@purdue.edu>
2023-04-18 15:02:32 -0400

    Wi-Fi filtering, About page, and Claiming business

--- src/pages/categories/Categories.js
+++ src/pages/categories/Categories.js
@@ -8,6 +8,8 @@ function Categories() {
   const [places, setPlaces] = useState([])
   const [trending, setTrending] = useState([])
   const [displayTrending, setDisplayTrending] = useState(false)
+  const [selectedTags, setSelectedTags] = useState([]);
+  const [showTags, setShowTags] = useState(false);
 
   const handleTrendingClick = () => {
     setDisplayTrending(!displayTrending)
@@ -21,14 +23,32 @@ function Categories() {
         console.log(response.data)
         console.log("Trending here")
         console.log(trendingData.data)
-        setPlaces(response.data);
+        let sortedData = response.data;
+        if (selectedTags.length > 0) {
+          sortedData = sortedData.filter(place => {
+            return selectedTags.every(tag => place.tags.includes(tag));
+          });
+        }
+        setPlaces(sortedData);
         setTrending(trendingData.data)
       } catch (error) {
         console.error(error);
       }
     }
     fetchData();
-  }, []);
+  }, [selectedTags]);
+
+  const handleTagSelection = (tag) => {
+    if (selectedTags.includes(tag)) {
+      setSelectedTags(selectedTags.filter(t => t !== tag));
+    } else {
+      setSelectedTags([...selectedTags, tag]);
+    }
+  };
+
+  const isTagSelected = tag => {
+    return selectedTags.includes(tag);
+  };
 
   return (
     <div>
@@ -42,6 +62,12 @@ function Categories() {
         <Link to='/categories/residence-halls'><button className='filter-button'>Residence Halls</button></Link>
         <Link to='/categories/study-spots'><button id='right-filter' className='filter-button'>Study Spots</button></Link>
       </div>
+      <button className={showTags === true ? 'showtags' : ''} onClick={() => { setShowTags(!showTags); setSelectedTags([]) }}>Filter by tags</button>
+        {showTags && (
+          <div className="tag-selector">
+            <button className={isTagSelected('Wi-Fi') ? 'selected' : ''} onClick={() => handleTagSelection('Wi-Fi')}>Wi-Fi</button>
+          </div>
+        )}
       <div className='sorting-container'>
         <span id='all-places'>All Places {displayTrending ? '>' : ''} {displayTrending ? 'Trending' : ''}</span>
         <button className="fire-button" onClick={handleTrendingClick}>
